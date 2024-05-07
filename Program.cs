0728e12d52b47837b7413d5798ecda6c9c4b27ee
Danila Matrenov <djslash1337@gmail.com>
2024-05-07 21:07:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Введите текст:");
+string[] textMass;
+string text = Console.ReadLine();
+textMass = text.Split(' ');
+Console.WriteLine("Количество слов:");
+Console.WriteLine(textMass.Length);
+Console.WriteLine("Слова с заглавной буквы");
+for (int i = 0;i< textMass.Length; i++)
+{
+    TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+    textMass[i] = textInfo.ToTitleCase(textMass[i]);
+    Console.WriteLine(textMass[i]);   
+}
